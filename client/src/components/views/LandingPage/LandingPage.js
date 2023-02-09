d2dea4a025a4e077065d02c54a89b4cfdd4d1b8b
seulGit <seulgi9421@gmail.com>
2023-02-09 15:51:11 +0900

    230209 gridcards 완성/loadmore btn 완성

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -9,19 +9,31 @@ function LandingPage() {
 
     const [Movies, setMovies] = useState([])
     const [MainMovieImage, setMainMovieImage] = useState(null)
+    const [CurrentPage, setCurrentPage] = useState(0);
 
     useEffect(() => {
         const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+        fetchMovies(endpoint)
 
+    }, [])
+
+    const fetchMovies = (endpoint ) => {
         fetch(endpoint)
         .then(response => response.json())
         .then(response => {
             console.log(response);
-            setMovies([...response.results])
-            setMainMovieImage(response.results[0])
+            setMovies([...Movies, ...response.results])
+            if (CurrentPage === 0) {
+                setMainMovieImage(response.results[0])
+            }
+            setCurrentPage(response.page)
         })
+    }
 
-    }, [])
+    const loadMoreItems = () => {
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${CurrentPage + 1}`;
+        fetchMovies(endpoint)
+    }
 
     return (
         <div style={{ width: '100%', margin: '0' }}>
@@ -29,9 +41,9 @@ function LandingPage() {
             {/* Main Image */}
             {MainMovieImage &&
                 <MainImage 
-                image={`${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}`}
-                title={MainMovieImage.original_title}
-                text={MainMovieImage.overview} 
+                    image={`${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}`}
+                    title={MainMovieImage.original_title}
+                    text={MainMovieImage.overview} 
                 />
             }
 
@@ -41,15 +53,15 @@ function LandingPage() {
 
                 {/* Movie Grid Cards */}
 
-                <Row>
+                <Row gutter={[16, 16]}>
 
                 {Movies && Movies.map((movie, index) => (
                     <React.Fragment key={index}>
                         <GridCards
                             image={movie.poster_path ?
                             `${IMAGE_BASE_URL}w500${movie.poster_path}` : null} 
-                             movieId={movie.id}
-                             movieName={movie.original_title}
+                            movieId={movie.id}
+                            movieName={movie.original_title}
                         />
                     </React.Fragment>
                 ))}
@@ -59,7 +71,7 @@ function LandingPage() {
             </div>
 
             <div style={{ display: 'flex', justifyContent: 'center' }}>
-                <button> Load More </button>
+                <button onClick={loadMoreItems}> Load More </button>
             </div>
             
         </div>
--- client/src/components/views/commons/GridCards.js
+++ client/src/components/views/commons/GridCards.js
@@ -6,7 +6,7 @@ function GridCards(props) {
     <Col lg={6} md={8} xs={24}>
         <div style={{ position: 'relative' }}>
             <a href={`/movie/${props.movieId}`}>
-                <img src={props.image} alt={props.movieName} />
+                <img style={{width: '100%', height: '320px' }} src={props.image} alt={props.movieName} />
             </a>
         </div>
     </Col>
